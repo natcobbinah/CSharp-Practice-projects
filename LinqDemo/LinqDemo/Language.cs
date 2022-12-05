8c1494339b84cbc712ac167071fd20de82bc5565
natcobbinah <baby.hugo@yahoo.com>
2022-12-06 00:20:40 +0100

    LINQdemo added

---
+++ LinqDemo/LinqDemo/Language.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    class Language
+    {
+        public int Year { get; set; }
+        public string Name { get; set; }
+
+        public string ChiefDeveloper { get; set; }
+
+        public string Predecessors { get; set; }
+
+        public Language(int year, string name, string chiefDeveloper, string predecessors)
+        {
+            Year = year;
+            Name = name;
+            ChiefDeveloper = chiefDeveloper;
+            Predecessors = predecessors;
+        }
+
+        public string Prettify()
+        {
+            return $"{Year}, {Name}, {ChiefDeveloper}, {Predecessors}";
+        }
+
+        public static void PrettyPrintAll(IEnumerable<Language> languages)
+        {
+            foreach(var lang in languages)
+            {
+                Console.WriteLine($"{lang.Year}, {lang.Name}, {lang.ChiefDeveloper}, {lang.Predecessors}");
+            }
+        }
+
+        public static Language FromTsv(string tsvLine)
+        {
+            string[] values = tsvLine.Split('\t');
+            Language language = new Language(
+                    Convert.ToInt32(values[0]),
+                    Convert.ToString(values[1]),
+                    Convert.ToString(values[2]),
+                    Convert.ToString(values[3])
+                );
+          return language;
+        }
+    }
+}
---
+++ LinqDemo/LinqDemo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Language> languages = File.ReadAllLines("./languages.tsv")
+                                .Skip(1)
+                                .Select(line => Language.FromTsv(line))
+                                .ToList();
+
+            //print all text in tsv(tab separated values file)
+            /* foreach(var lang in languages)
+             {
+                 Console.WriteLine(lang.Prettify());
+             }*/
+
+            var selectAttributes = languages.Select(lang => $"{lang.Name} {lang.Year} {lang.ChiefDeveloper}");
+            /*foreach(var lang in selectAttributes)
+             {
+                 Console.WriteLine(lang);
+             }*/
+
+            var findCSharp = languages.Where(lang => lang.Name == "C#");
+            /*foreach(var lang in findCSharp)
+            {
+                Console.WriteLine(lang.Prettify());
+            }*/
+
+            var microsoftLangs = languages.Where(lang => lang.ChiefDeveloper
+                    .Contains("Microsoft"));
+            /* foreach(var lang in microsoftLangs)
+             {
+                 Console.WriteLine(lang.Prettify());
+             }*/
+
+            var langPredecessors = languages.Where(lang => lang.Predecessors
+                    .Contains("Lisp"));
+            /*foreach(var lang in langPredecessors)
+            {
+                Console.WriteLine(lang.Prettify());
+            }*/
+
+            var scriptLangs = languages.Where(lang => lang.Name.Contains("Script"))
+                                       .Select(lang => lang.Name);
+            /*foreach(var lang in scriptLangs)
+            {
+                Console.WriteLine(lang);
+            }*/
+
+            int langCount = languages.Count();
+            //Console.WriteLine(langCount);
+
+            int langCountBtwn1995and2000 = languages.Where(lang => lang.Year >= 1995 && lang.Year <= 2005)
+                                                    .Count();
+            // Console.WriteLine(langCountBtwn1995and2000);
+
+            var nearMilleniumLangs = languages.Where(lang => lang.Year >= 1995 && lang.Year <= 2005)
+                                              .Select(lang => $"{lang.Name} was invented in {lang.Year}");
+            foreach(var lang in nearMilleniumLangs)
+            {
+                Console.WriteLine(lang);
+            }
+        }
+    }
+}
